3e1ea23217702c08578a407162852cd00cf9c939
Felipe Moreno <pipemon@mit.edu>
2018-05-07 21:46:57 -0400

    Rough text file reader

--- NetworkParser.py
+++ NetworkParser.py
@@ -1,5 +1,13 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
+import os
 
 # this class takes in a text file representing a network and parses it into a graphframe representation
-sc = SparkContext(master= 'local[2]')
-textFile = sc.textFile('./socfb-American75.mtx')
+conf = SparkConf().setMaster("local").setAppName("Load Facebook Network")
+sc = SparkContext(conf = conf)
+path = "/network-examples/example.txt"
+textFile = sc.textFile(path)
+textFile.collect()
+
+
+#data = [1, 2, 3, 4, 5]
+#distData = sc.parallelize(data)
